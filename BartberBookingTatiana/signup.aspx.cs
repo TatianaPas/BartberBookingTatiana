5fa660df19d6f6c903f0550a49517c1279d07a3b
Tatiana Pasechnik <81549043+TatianaPas@users.noreply.github.com>
2022-04-17 10:57:22 +1200

    Update signup.aspx.cs
    connect database

--- BartberBookingTatiana/signup.aspx.cs
+++ BartberBookingTatiana/signup.aspx.cs
@@ -1,65 +1,40 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Data.SqlClient;
+using System.Data;
 using System.Configuration;
+using MySql.Data.MySqlClient;
 
 namespace BartberBookingTatiana
 {
     public partial class signup : System.Web.UI.Page
-
+        
+        
     {
-       // string connection = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
-       
         protected void Page_Load(object sender, EventArgs e)
         {
+           
 
         }
 
-       
-
-        // protected void signupButton_Click(object sender, EventArgs e)
-        // {
-
-        // try
-        //{
-
-
-        //  SqlCommand cmd = new SqlCommand("insert into Clients(FirstName,LastName,Email,Phone,Password,AccountStatus) values(@FirstName,@LastName,@Email,@Phone,@Password,@AccountStatus)", con);
-
-        //  cmd.Parameters.AddWithValue("@FirstName", firstNameTextBox.Text.Trim());
-        //  cmd.Parameters.AddWithValue("@LastName", lastNameTextBox.Text.Trim());
-        //  cmd.Parameters.AddWithValue("@Email", emailTextBox.Text.Trim());
-        // cmd.Parameters.AddWithValue("@Phone", phoneTextBox.Text.Trim());
-        //cmd.Parameters.AddWithValue("@Password", passwordTextBox.Text.Trim());
-        //   cmd.Parameters.AddWithValue("@AccountStatus", "active");
-
-        //cmd.ExecuteNonQuery();
-        // con.Close();
-        // Response.Write("SignUp sucessfully");
-
-        // }
-        // catch (Exception ex)
-        // {
-        //     Response.Write(ex.ToString());
-        // }
-
-
-
-
-        //  }
-
-        //  protected void firstNameTextBox_TextChanged(object sender, EventArgs e)
-        //  {
-
-        //  }
-
-        //  protected void SqlDataSource1_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
-        // {
-
-        // }
+        protected void signupButton_Click(object sender, EventArgs e)
+        {
+            string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            using (MySqlConnection con = new MySqlConnection(constr))
+            {
+                
+                MySqlCommand cmd;
+                string str;
+                string status = "active";
+                
+                con.Open();
+                str = "insert into Customers values (NULL,'" + firstNameTextBox.Text + "','" + lastNameTextBox.Text + "','" + emailTextBox.Text + "','" + phoneTextBox.Text + "','" + PasswordTextBox.Text + "','" + status + "')";
+                cmd = new MySqlCommand(str, con);
+                cmd.ExecuteNonQuery();
+            }
+        }
     }
 }
